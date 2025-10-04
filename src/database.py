542533dac659d6ced5f358a50171b692c4a9d561
Dmitriy <prizrak2165@gmail.com>
2025-10-04 14:17:58 +0300

    Добавлена папка миграций и добавлена class Base в database

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
@@ -9,3 +10,7 @@ engine = create_async_engine(settings.DB_URL)
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 session = async_session_maker()
+
+
+class Base(DeclarativeBase):
+    pass
