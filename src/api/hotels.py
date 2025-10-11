68fb5a6c594f878aafc90e5d47960e029d6f39ac
Dmitriy <prizrak2165@gmail.com>
2025-10-12 01:45:35 +0300

    Реализована ручка для получения отеля по его id

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -26,6 +26,12 @@ async def get_hotels(
         )
 
 
+@router.get("/{hotel_id}")
+async def get_hotel_by_id(hotel_id: int):
+    async with async_session_maker() as session:
+        hotel = await HotelsRepository(session).get_one_or_none(id=hotel_id)
+        return hotel
+
 @router.post("")
 async def create_hotel(
         hotel_data: Hotel = Body(
