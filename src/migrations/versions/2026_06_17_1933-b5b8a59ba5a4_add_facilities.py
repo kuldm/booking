5f1be0d27fcff8b6492398126f6be0b890b80ba8
Kultyshev Dmitrii <kultyshev@nobr.ru>
2026-06-17 21:04:45 +0300

    Добавлены модели для удобств(facilities) и m2m модель для связи удобств и комнат. А Так же добавлена миграция с жтии данными

---
+++ src/migrations/versions/2026_06_17_1933-b5b8a59ba5a4_add_facilities.py
@@ -0,0 +1,49 @@
+"""add facilities
+
+Revision ID: b5b8a59ba5a4
+Revises: 177a9bd5f064
+Create Date: 2026-06-17 19:33:23.272291
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "b5b8a59ba5a4"
+down_revision: Union[str, Sequence[str], None] = "177a9bd5f064"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "facilities_rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+
+def downgrade() -> None:
+    op.drop_table("facilities_rooms")
+    op.drop_table("facilities")
---
+++ src/models/facilities.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+
+from src.database import Base
+
+
+class FacilitiesModels(Base):
+    __tablename__ = "facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+
+
+
+class RoomsFacilitiesModels(Base):
+    __tablename__ = "facilities_rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
+    facility_id: Mapped[int] = mapped_column(ForeignKey("facilities.id"))
