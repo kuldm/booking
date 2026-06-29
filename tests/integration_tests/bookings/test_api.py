3302e9ea2254ebb7fd373c417c20232741e966af
Kultyshev Dmitrii <kultyshev@nobr.ru>
2026-06-29 18:37:39 +0300

    Добавлен тест на проверку бронирований у авторизированного пользователя

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,5 +1,7 @@
 import pytest
 
+from tests.conftest import get_db_null_pool
+
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
     (1, "2025-08-01", "2025-08-10", 200),
@@ -11,8 +13,12 @@ import pytest
     (1, "2025-09-01", "2025-09-10", 200),
 ])
 async def test_add_booking(
-        room_id, date_from, date_to, status_code,
-        db, authenticated_ac
+        room_id,
+        date_from,
+        date_to,
+        status_code,
+        db,
+        authenticated_ac
 ):
     room_id = (await db.rooms.get_all())[0].id
     response = await authenticated_ac.post(
@@ -29,3 +35,41 @@ async def test_add_booking(
         assert isinstance(res, dict)
         assert res["status"] == "OK"
         assert "data" in res
+
+
+# Как вариант можно вынести в conftest и назвать функцию иначе, чтобы использовать её в других тестах на уровне модуля
+@pytest.fixture(scope="module")
+async def delete_all_bookings() -> None:
+    async for _db in get_db_null_pool():
+        await _db.bookings.delete()
+        await _db.session.commit()
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, booked_rooms", [
+    (1, "2025-08-01", "2025-08-10", 1),
+    (1, "2025-08-01", "2025-08-10", 2),
+    (1, "2025-08-01", "2025-08-10", 3),
+])
+async def test_add_and_get_my_bookings(
+        room_id,
+        date_from,
+        date_to,
+        booked_rooms,
+        delete_all_bookings,
+        authenticated_ac,
+):
+    response = await authenticated_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response.status_code == 200
+
+    response_my_bookings = await authenticated_ac.get(
+        "/bookings/me",
+    )
+    assert response_my_bookings.status_code == 200
+    assert len(response_my_bookings.json()) == booked_rooms
